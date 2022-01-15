51614085f781ac38c8607fc73bc16a9b4a1535f1
MaxZabol <Thinkingmaximus@gmail.com>
2022-01-15 17:12:47 -0500

    added . gitignor file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,4 +3,8 @@ public class HelloWorld {
     //do some work
     // ggfhghfgh
     //// гшег
+    //new sentnce
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+    }
 }
